96949bdaf2a7269cb754fdeb323057e4f4e6714b
kyamaura <kyamaura@netprotections.co.jp>
2018-10-18 16:16:35 +0900

    過去の日付のタスクを登録できないように修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,13 @@ class Task < ActiveRecord::Base
   validates :reception_deadline, {presence: true}
   validates :content, { length:{maximum:140}}
   validates :user_id, {presence: true}
+  validate :reception_deadline_cannot_be_in_the_past
+
+def reception_deadline_cannot_be_in_the_past
+  if reception_deadline < Date.today
+    errors.add(:reception_deadline, ": 過去の日付は使用できません")
+  end
+end
 
 def user
   return User.find_by(id: self.user_id)
