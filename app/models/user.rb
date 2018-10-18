2c0358a7725a8c362549779fd3f5568f22dca470
kyamaura <kyamaura@netprotections.co.jp>
2018-10-18 16:21:17 +0900

    不正なアドレスの登録を弾く

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :name , {presence: true}
-  validates :email , {presence: true, uniqueness: true}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: {with: VALID_EMAIL_REGEX}, allow_blank: true
+  validates :email, presence: true
   validates :password , {presence: true}
 
 def tasks
